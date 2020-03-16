86ed618e60c259d3ea8e08485b370be59db63f55
Vladislav Zhigalov <36502032+Vladqwelkj@users.noreply.github.com>
2020-03-17 00:33:36 +0700

    Update views.py

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 def index():
     with open('templates/index.html') as template:
-        return tempalte.read()
+        return template.read()
 
 def blog():
     with open('templates/blog.html') as template:
